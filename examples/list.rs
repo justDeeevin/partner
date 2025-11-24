c186b6998b4bc8fb6bad4a68d24d2cb5121a6e1b
Devin Droddy <devin@justdeeevin.dev>
2025-11-23 23:35:22 -0500

    lib: list devices/device example

---
+++ examples/list.rs
@@ -0,0 +1,16 @@
+use std::path::PathBuf;
+
+use partner::Device;
+
+fn main() {
+    let devices = Device::get_all().unwrap();
+    if let Some(device) = std::env::args().nth(1) {
+        let device = devices
+            .into_iter()
+            .find(|d| d.path() == PathBuf::from(&device))
+            .unwrap_or_else(|| Device::open(device).unwrap());
+        dbg!(device);
+    } else {
+        dbg!(devices);
+    }
+}
